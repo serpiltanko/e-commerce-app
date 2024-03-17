7a6ee8f3422e553238c524f7b0ae372810044ab0
serpiltanko <serpiltanko@gmail.com>
2024-03-17 14:03:34 +0300

    firebase-authentication added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, SafeAreaView } from "react-native";
+import React from "react";
+import { AntDesign } from "@expo/vector-icons";
 
 const SearchBar = () => {
   return (
-    <View>
-      <Text>SearchBar</Text>
-    </View>
-  )
-}
+  
+      <SafeAreaView style={styles.container}>
+        <AntDesign
+          style={styles.iconStyle}
+          name="search1"
+          size={24}
+          color="black"
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Ara"
+          autoCorrect={false}
+        />
+      </SafeAreaView>
+   
+  );
+};
 
-export default SearchBar
+export default SearchBar;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "lightgray",
+    flexDirection: "row",
+    marginTop: 70,
+    marginHorizontal:15,
+
+    height: 50,
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  iconStyle: {
+    marginHorizontal: 15,
+  },
+  textInput: {
+    flex: 1,
+    fontSize: 18,
+  },
+});
--- src/routes/MyBottomTabNavigator.js
+++ src/routes/MyBottomTabNavigator.js
@@ -4,15 +4,66 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import MyCartScreen from "../screens/MyCartScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const MyBottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="MyCart" component={MyCartScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: "black",
+        tabBarLabelStyle: { fontSize: 20 }
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          title: "Anasayfa",
+          tabBarLabelStyle: { fontSize: 13 },
+
+          tabBarIcon: () => {
+            return (
+              <View>
+                <AntDesign name="home" size={26} color="black" />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="MyCart"
+        component={MyCartScreen}
+        options={{
+          title: "Sepetim",
+          tabBarLabelStyle: { fontSize: 13 },
+          tabBarIcon: () => {
+            return (
+              <View>
+                <AntDesign name="shoppingcart" size={26} color="black" />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          title: "Profilim",
+          tabBarLabelStyle: { fontSize: 13 },
+          tabBarIcon: () => {
+            return (
+              <View>
+                <FontAwesome6 name="user" size={24} color="black" />
+              </View>
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
